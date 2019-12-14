a59ae48f538bf7ff63b555bbae7973a9361ae44f
Peter Olsen <pto@me.com>
2019-12-14 07:56:14 -0500

    remove API key

--- DCTrains WatchKit Extension/ApiKey.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ApiKey.swift
-//  DCTrains
-//
-//  Created by Peter Olsen on 12/19/15.
-//  Copyright © 2015 Peter Olsen. All rights reserved.
-//
-
-let api_key = "27ec81dd9fa941a1a4ef0d84e3cf9cff"
